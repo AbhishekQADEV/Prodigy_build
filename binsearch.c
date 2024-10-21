3a6b3373d977a827ae52c8c884b929f2c8db74d7
AbhishekQADEV <abhipokiya8160@gmail.com>
2024-10-21 14:31:30 +0530

    Conductor generated Project instruction

---
+++ binsearch.c
@@ -0,0 +1,37 @@
+```markdown
+# Test Prodigybuild
+
+Welcome to Test Prodigybuild. This repository is a collection of C programming algorithm files. We have a set of different algorithms implemented such as Binary Search Trees, Double Linked List, Insertion Deletion in Linked Lists, and several common search and sort algorithms. Feel free to contribute!
+
+## Software Requirements
+This project is developed using C programming language. The list of software requirements are:
+
+1. Any Linux/Unix operating system or Windows operating system, and 'gcc' Compiler. You can install it in Ubuntu by using the following command.
+   `sudo apt-get install build-essential`
+
+2. Git, for version control. You can download it from the [OFFICIAL WEBSITE](https://git-scm.com/downloads)
+
+## Project Setup & Local Environment
+
+### For Linux/Unix
+1. Open your terminal.
+2. Clone the repository by running the command `git clone <repository_link>`.
+3. Navigate to the cloned repository's folder by running `cd <repository_name>`.
+4. To compile any C file, use the command `gcc -o outputfilename filename.c`. For example, if the file name is 'BST.c', the command will be `gcc -o bst BST.c`.
+5. To run the compiled file, use the command `./outputfilename`. Continuing from the above example, it would be `./bst`.
+
+### For Windows
+1. Open your command prompt.
+2. Clone the repository by running the command `git clone <repository_link>`.
+3. Navigate to the cloned repository's folder by running `cd <repository_name>`.
+4. To compile any C file, use the command `gcc -o outputfilename.exe filename.c`. For example, if the file name is 'BST.c', the command will be `gcc -o bst.exe BST.c`.
+5. To run the compiled file, use the command `outputfilename`. Continuing from the above example, it would be `bst`.
+
+## CI/CD 
+
+For the CI/CD pipeline, please refer to the CONTRIBUTING.md document which provides detailed steps for deployment.
+
+## Contribute 
+
+Feel free to dive in! [Open an issue](https://github.com/<your_username>/<repository_name>/issues/new) or submit PRs.
+```
