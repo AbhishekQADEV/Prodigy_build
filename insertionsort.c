c566088af195ac74d58bd4151f36b4c80afbfdba
AbhishekQADEV <abhipokiya8160@gmail.com>
2024-10-22 14:50:19 +0530

    GITT-51 (task) new

--- insertionsort.c
+++ insertionsort.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+void bubble_sort(int *array, int len)
 {
-    int *unsorted[] = {5, 6, 4, 3, 9};
-    int *sorted[5];
-    do
+    for(int i = 0; i < len; i++)
     {
-        int a = 0;
-        a++;
-        int b = 0;
-        b++;
-        
-        if(unsorted[a] < unsorted[b])
+        for(int j = 0; j < len - i - 1; j++)
         {
-            sorted[a] = unsorted[a];
+            if(array[j] > array[j + 1])
+            {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
         }
-    } while(sizeof(sorted) != sizeof(unsorted));
+    }
+}
+
+int main(void)
+{
+    int yarr[] = {3, 9, 4, 8, 7, 6, 1, 2, 0, 10};
+    bubble_sort(yarr, 10);
+    printf("[");
+    for(int i = 0; i < 10; i++)
+        printf("%d, ", yarr[i]);
+    printf("\b\b]\n");
+    return 0;
 }
